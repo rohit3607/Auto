d311bd5e504672b10af94f82c61306d07d2b1aa1
aalaksh <aallkkaa9451@gmail.com>
2024-11-04 14:49:33 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -122,15 +122,15 @@ REPLY_ERROR = """<code>Use this command as a replay to any telegram message with
 async def not_joined(client: Client, message: Message):
     buttons = [
         [
-            InlineKeyboardButton(text="• ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
-            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ •", url=client.invitelink2),
+            InlineKeyboardButton(text="Join Channel 1", url=client.invitelink),
+            InlineKeyboardButton(text="Join Channel 2", url=client.invitelink2),
         ]
     ]
     try:
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text='• ᴛʀʏ ᴀɢᴀɪɴ •',
+                    text='Try Again',
                     url=f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]

f989337f45688e3faf4e4259b50ae571e05ab99f
rohit3607 <rohitplayer87089@gmail.com>
2024-11-17 22:42:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -65,7 +65,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = None
+USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!\n\n» ᴍʏ ᴏᴡɴᴇʀ : @javpostr"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
